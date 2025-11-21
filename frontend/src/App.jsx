0e07554c6e334b6ef92fd4730a8a0dab8d3d992e
MdWaseem786 <mwaseem19687@gmail.com>
2025-11-21 22:49:24 +0530

    fixed login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { Navbar } from "./components/Navbar";
+import  Navbar  from "./components/Navbar";
 import { HomePage } from "./pages/HomePage";
-import { LoginPage } from "./pages/LoginPage";
+import  LoginPage  from "./pages/LoginPage";
 import { SignUpPage } from "./pages/SignUpPage";
 import { ProfilePage } from "./pages/ProfilePage";
 import { SettingsPage } from "./pages/SettingsPage";
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -40,13 +40,28 @@ export const useAuthStore = create((set) => ({
   },
   logout: async(  ) => {
     try {
-      await axiosInstance.post("/auth/logout");
+      await axiosInstance.post("/auth/signout");
       set({ authUser: null });
       toast.success("Logged out successfully");
     } catch (error) {
       toast.error(error.response?.data?.message || "Logout Failed");
     }
   },
+  login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await axiosInstance.post("/auth/signin", data);
+      set({ authUser: res.data });
+      toast.success("Logged in successfully");
+      return true;
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Login failed");
+      return false;
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
+  
   
 }));
 
